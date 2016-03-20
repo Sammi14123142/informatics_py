ca5da2cafbeab2c53473c02a9e3e1f556bec9659
Joey14123142 <14123142d@connect.polyu.hk>
2016-03-20 09:32:40 +0800

    map

---
+++ geodata/where.js
@@ -0,0 +1,9 @@
+myData = [
+[22.2840507,114.1378391, 'The University of Hong Kong Main Building, Pok Fu Lam Rd, Lung Fu Shan, Hong Kong'],
+[22.417534,114.20383, 'Chinese University, Ma Liu Shui, Hong Kong'],
+[22.3354512,114.1829159, 'Baptist University Rd, Kowloon Tsai, Hong Kong'],
+[22.270961,114.123784, 'Duchess Of Kent Childrens Hospital At Sandy Bay, Sandy Bay, Hong Kong'],
+[22.396428,114.109497, 'Hong Kong'],
+[22.396428,114.109497, 'Hong Kong'],
+[52.3718826,5.2545349, '1326 HK Almere, Netherlands']
+];
