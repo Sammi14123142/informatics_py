225ec0d433df1f5f6b63e54ea2c784568b88dcd1
Sammi14123142 <14123142d@connect.polyu.hk>
2016-05-16 17:09:48 +0800

    +socket

--- re_exer.py
+++ re_exer.py
@@ -1,14 +1,23 @@
 import os
 os.chdir("C:\\Users\Library\Downloads\code")
 
-import re
+import re        
 hand=open('regex_sum_42.txt')
-n=0
+sum=0
 for line in hand:
     line=line.rstrip()
     numbers=re.findall('[0-9]+',line)
-    for num in numbers:
-        if len(num)>0:
-            n = int(num)
-            n+=n
-    print(n)
+for num in numbers:
+    if len(num)>0:
+        sum+= int(num)
+print(sum)
+
+hand=open('regex_sum_197283.txt')
+sum=0
+for line in hand:
+    line=line.rstrip()
+    numbers=re.findall('[0-9]+',line)
+for num in numbers:
+    if len(num)>0:
+        sum+= int(num)
+print(sum)
