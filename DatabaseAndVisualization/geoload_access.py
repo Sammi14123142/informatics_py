b14d3b546d53d6620d73dae78a2f9e17f4328963
Sammi14123142 <14123142d@connect.polyu.hk>
2016-06-01 12:30:22 +0800

    To MS access

--- DatabaseAndVisualization/geoload_access.py
+++ DatabaseAndVisualization/geoload_access.py
@@ -1,5 +1,5 @@
 import urllib
-import sqlite3
+import pyodbc
 import json
 import time
 import ssl
@@ -10,7 +10,8 @@ serviceurl = "http://maps.googleapis.com/maps/api/geocode/json?"
 # scontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
 scontext = None
 
-conn = sqlite3.connect('geodata.sqlite')
+DBfile = 'geodata.accdb'
+conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ='+DBfile)
 cur = conn.cursor()
 
 cur.execute('''
@@ -21,22 +22,22 @@ count = 0
 for line in fh:
     if count > 200 : break
     address = line.strip()
-    print ''
+    print('')
     cur.execute("SELECT geodata FROM Locations WHERE address= ?", (buffer(address), ))
 
     try:
         data = cur.fetchone()[0]
-        print "Found in database ",address
+        print("Found in database ",address)
         continue
     except:
         pass
 
-    print 'Resolving', address
+    print('Resolving', address)
     url = serviceurl + urllib.urlencode({"sensor":"false", "address": address})
-    print 'Retrieving', url
+    print('Retrieving', url)
     uh = urllib.urlopen(url, context=scontext)
     data = uh.read()
-    print 'Retrieved',len(data),'characters',data[:20].replace('\n',' ')
+    print('Retrieved',len(data),'characters',data[:20].replace('\n',' '))
     count = count + 1
     try: 
         js = json.loads(str(data))
@@ -45,8 +46,8 @@ for line in fh:
         continue
 
     if 'status' not in js or (js['status'] != 'OK' and js['status'] != 'ZERO_RESULTS') : 
-        print '==== Failure To Retrieve ===='
-        print data
+        print('==== Failure To Retrieve ====')
+        print(data)
         break
 
     cur.execute('''INSERT INTO Locations (address, geodata) 
@@ -54,4 +55,3 @@ for line in fh:
     conn.commit() 
     time.sleep(1)
 
-print "Run geodump.py to read the data from the database so you can visualize it on a map."
